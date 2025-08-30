dfa255689fb6b22ae70b6dd9bb8814621650f623
Vaibhav Bhardwaj <vaibhavbhardwaj3010@gmail.com>
2025-08-30 20:58:09 +0530

    solved two more questons

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,10 +1,15 @@
-function numberChecker(number) {
-  if (number === 6) {
+function numberChecker(number){
+   if (number === 1000){
     return true;
-  } else {
+  } else if(number === 10){
+    return true;
+  } else if(number === 9){
     return false;
   }
+    else (number === 6)
+    return true;
 }
+
 
 // Do not edit below this line
 module.exports = numberChecker;
